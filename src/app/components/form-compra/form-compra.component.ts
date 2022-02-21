a3164b76dcc14ff051a814a2c010cba8166ad0a1
Arnaiz23 <aaf110501@gmail.com>
2022-02-21 20:25:45 +0000

    Añadir direccion al comprar OK

--- src/app/components/form-compra/form-compra.component.ts
+++ src/app/components/form-compra/form-compra.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Usuario } from 'src/app/models/usuario';
 import { ProductosService } from 'src/app/service/productos.service';
+import { UsuarioService } from 'src/app/service/usuario.service';
+import swal from "sweetalert";
 
 @Component({
   selector: 'app-form-compra',
@@ -11,10 +14,13 @@ export class FormCompraComponent implements OnInit {
 
   public carrito!: Array<any>;
   public data: any;
+  public usuario!: Usuario;
+  public updateUser!: Usuario;
 
   constructor(
     private _productoService: ProductosService,
-    private _router: Router
+    private _router: Router,
+    private _usuarioService: UsuarioService
   ) { 
     this.data = {
       calle: "",
@@ -28,17 +34,48 @@ export class FormCompraComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.recogerUsuario();
+  }
+
+  recogerUsuario(){
+    this._usuarioService.identidad().subscribe(
+      response =>{
+        // console.log(response.usuario[0])
+        this.usuario = response.usuario[0];
+      },
+      err =>{
+        console.log(err.error)
+      }
+    )
   }
 
   finalizarCompra() {
     this.carrito = this._productoService.devolverCarrito();
-    
-    alert("Compra exitosa");
-    localStorage.removeItem("carrito");
-    this._router.navigate(['/']);
 
-    // Meterle una alerta de las guapas
-    // Crear BBDD de compras con (_idCompra, dataCompra, _idUsuario)
+    let direcciones = this.usuario.direcciones;
+
+    let direccion = { "calle" : this.data.calle};
+
+    direcciones.push(direccion);
+
+    this.updateUser = new Usuario(this.usuario._id,this.usuario.usuario,this.usuario.password,this.usuario.tipo,this.usuario.nombre,this.usuario.apellidos,this.usuario.correo,direcciones);
+
+    this._usuarioService.updateUser(this.usuario._id,this.updateUser).subscribe(
+      response => {
+        // console.log(response);
+        swal("Exito","Compra realizada con exito","success");
+        localStorage.removeItem("carrito");
+        this._router.navigate(['/']);
+      },
+      error =>{
+        // console.log(error);
+        swal("Error","No se ha podido guardar la direccion","danger");
+      }
+    );
+
+
+    // ! Coger la calle de data.calle y guardarla en el usuario
+    // ! Crear BBDD de compras con (_idCompra, dataCompra, _idUsuario)
   }
 
 }
