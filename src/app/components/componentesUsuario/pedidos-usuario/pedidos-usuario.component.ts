46c177c4aaeca427145cdcd219b3ec210dceb06b
Arnaiz23 <aaf110501@gmail.com>
2022-02-22 11:10:15 +0000

    getPedidos OK

--- src/app/components/componentesUsuario/pedidos-usuario/pedidos-usuario.component.ts
+++ src/app/components/componentesUsuario/pedidos-usuario/pedidos-usuario.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Pedido } from 'src/app/models/pedido';
+import { Pedido, Pedidos } from 'src/app/models/pedido';
 import { Usuario } from 'src/app/models/usuario';
 import { PedidosService } from 'src/app/service/pedidos.service';
 import { UsuarioService } from 'src/app/service/usuario.service';
+import { global } from 'src/app/service/global';
 
 @Component({
   selector: 'app-pedidos-usuario',
@@ -17,7 +18,9 @@ export class PedidosUsuarioComponent implements OnInit {
   }
 
   public usuario!: Usuario;
-  public pedidos !: Pedido[];
+  public pedidos !: Pedidos[];
+  public pedido !: Pedido[];
+  public url!: string;
 
   constructor(
     private _usuarioService: UsuarioService,
@@ -25,6 +28,7 @@ export class PedidosUsuarioComponent implements OnInit {
     private _pedidoService: PedidosService
   ) { 
     if(this.currentUser == undefined) _router.navigate(["/"]);
+    this.url = global.url;
   }
 
   ngOnInit(): void {
@@ -49,8 +53,24 @@ export class PedidosUsuarioComponent implements OnInit {
     this._pedidoService.getPedidos(id).subscribe(
       response => {
         // console.log(response);
-        this.pedidos = response.pedidos;
-        console.log(this.pedidos);
+        if(response.pedidos.length != 0){
+          this.pedidos = response.pedidos;
+          // console.log(this.pedidos);
+          let pedidoTotal:any = [];
+          this.pedidos.forEach(data => {
+            data.pedido.forEach(pedido => {
+              // console.log(pedido);
+              // this.pedido.push(pedido);
+              pedidoTotal.push(pedido);
+            })
+            // this.pedido.push(data.pedido)
+          })
+          this.pedido = pedidoTotal;
+          // console.log(this.pedido);
+        }
+        
+
+        // console.log(this.pedido);
       },
       error => {
         console.log(error);
